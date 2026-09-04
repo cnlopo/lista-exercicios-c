2332c39edaa57fa6f4b6f72003fc54708f715d61
cnlopo <rafaelmontalvao0007@hotmail.com>
2026-09-04 20:24:55 -0300

    Create ex04.c

---
+++ ex04.c
@@ -0,0 +1,5 @@
+/* EX03
+Leia 10 números inteiros e armazene-os em um vetor.
+a) Mostre todos os elementos do vetor. b) Calcule a soma dos elementos. c) Calcule a média. d) Identifique o maior valor. e) Identifique o menor valor.
+*/
+#include <stdio.h>
